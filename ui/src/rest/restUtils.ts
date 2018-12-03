6d760304cfbe53c5cee93757879250f1606cfc69
Tommi Soikkeli <soikkelitommi@gmail.com>
2018-12-03 23:06:54 +0200

    cors

--- ui/src/rest/restUtils.ts
+++ ui/src/rest/restUtils.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = 'localhost:8080/api';
 export const EMISSIONS_ENDPOINT = '/emissions';
-export const POPULATION_ENDPOINT = 'population';
+export const POPULATION_ENDPOINT = '/population';
 
 export const fetchDataFromUrl = (url: string, country: string) => {
   return fetch(`http://${API_BASE_URL}${url}?country=${country}`)
