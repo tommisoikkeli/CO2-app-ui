a716d59aec78d1e4e45415bafde1bcac3af0668e
Tommi Soikkeli <soikkelitommi@gmail.com>
2019-01-12 13:53:52 +0200

    rest url to heroku

--- src/rest/restUtils.ts
+++ src/rest/restUtils.ts
@@ -1,10 +1,10 @@
-export const API_BASE_URL = 'localhost:8080/api';
+export const API_BASE_URL = 'https://fierce-mountain-99549.herokuapp.com/api';
 export const EMISSIONS_ENDPOINT = '/emissions/all';
 export const PER_CAPITA_ENDPOINT = '/emissions/per_capita';
 
 export const fetchDataFromUrl = async (url: string, country: string) => {
   try {
-    const response = await fetch(`http://${API_BASE_URL}${url}?country=${country}`);
+    const response = await fetch(`${API_BASE_URL}${url}?country=${country}`);
     return response.json();
   } catch (e) {
     throw e;
